1ceeb1b86f3dc00c7bad78bcfc2f6dcbf0c8cc44
Rizvi Ahmed <48264629+rizvi111@users.noreply.github.com>
2023-03-31 19:52:24 +0600

    Update form-list.blade.php

--- resources/views/pages/form-list.blade.php
+++ resources/views/pages/form-list.blade.php
@@ -2,41 +2,52 @@
 
 @section('content')
 
-            <!-- End breadcum -->
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="title">
-                        <div class="title-body">
-                            <div class="table-responsive">
-                                <table class="table table-hover table-bordered" id="sampleTable">
-                                    <thead>
-                                        <tr>
-                                            <th>SL.</th>
-                                            <th>Date & time</th>
-                                            <th>Name</th>
-                                            <th>Email</th>
-                                            <th>Telephone</th>
-                                            <th>IP Address</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach($form_list->sortByDesc('created_at') as $flist)
-    <tr>
-        <td>{{ $loop->iteration }}</td>
-        <td>{{ $flist->created_at->format('Y-m-d H:i:s') }}</td>
-        <td>{{ $flist->fname .' '. $flist->sname }}</td>
-        <td>{{ $flist->email }}</td>
-        <td>{{ $flist->phone }} @if($flist->mobile), {{ $flist->mobile }}@endif</td>
-        <td>{{ $flist->ip_address }}</td>
-        <td><a class="btn btn-primary" href="{{ route('form.view', $flist->id) }}"><i class="nav-icon fa fa-eye"></i></a></td>
-    </tr>
-@endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
+<!-- End breadcum -->
+<div class="row">
+    <div class="col-md-12">
+        <div class="title">
+            <div class="title-body">
+                <div class="table-responsive">
+                    <table class="table table-hover table-bordered" id="sampleTable">
+                        <thead>
+                            <tr>
+                                <th>SL.</th>
+                                <th>Date & time</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Telephone</th>
+                                <th>IP Address</th>
+                                <th>Action</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($form_list->sortByDesc('created_at') as $flist)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $flist->created_at->format('Y-m-d H:i:s') }}</td>
+                                <td>{{ $flist->fname .' '. $flist->sname }}</td>
+                                <td>{{ $flist->email }}</td>
+                                <td>{{ $flist->phone }} @if($flist->mobile), {{ $flist->mobile }}@endif</td>
+                                <td>{{ $flist->ip_address }}</td>
+                                <td><a class="btn btn-primary" href="{{ route('form.view', $flist->id) }}"><i class="nav-icon fa fa-eye"></i></a></td>
+                                <td>
+  <form action="{{ route('form.delete', $flist->id) }}" method="POST">
+   @csrf
+   @method('DELETE')
+   <input type="hidden" name="_method" value="DELETE">
+   <button type="submit" class="btn btn-danger"><i class="nav-icon fa fa-trash"></i></button>
+</form>
+
+</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
+        </div>
+    </div>
+</div>
+
 @endsection
